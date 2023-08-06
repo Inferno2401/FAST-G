72fb0834162104b20385d5ae0ac0439fbd65e26e
Sanskar Shaurya <114353045+SanskarShaurya@users.noreply.github.com>
2023-08-06 16:44:24 +0530

    Update Graph.hpp
    Added a function for finding the perfect matching in case of unweighted bipartite graphs.

--- Graph.hpp
+++ Graph.hpp
@@ -3,6 +3,8 @@
 #include <queue>
 #include <utility>
 #include <stack>
+#include <algorithm>
+#include <limits>
 #include <unordered_set>
 #include <unordered_map>
 
@@ -19,10 +21,10 @@ public:
 
 class Graph
 {
-    protected:
-    std::vector<Node *> nodes; // The adjacency list of nodes of the graph
+protected:
+    std::vector<Node *> nodes;                              // The adjacency list of nodes of the graph
     std::vector<std::pair<int, std::pair<int, int>>> edges; // Vector containing all the edges and their weigths
-    Node *findNode(int val)    // Function to find the pointer of the node given its value
+    Node *findNode(int val)                                 // Function to find the pointer of the node given its value
     {
         for (Node *node : nodes)
         {
@@ -38,15 +40,15 @@ class Graph
 public:
     Graph();
     Graph(const Graph &graph);
-    void add_node(int data);                           // Adds a node in the adjacency list with the value given as parameter
-    void add_edge(int data1, int data2, int weight);   // Adds a weighted edge between the two nodes with the values as given in the argument
-    bool are_adjacent(int data1, int data2);           // Function to check whether two nodes with the given values are adjacent or not
-    int getWeight(int data1, int data2);               // Function to return the weight of edge between nodes given the node
-    void printGraph();                                 // Prints the entire graph (with weights in parenthesis)
-    Graph BFS(int start);                              // Returns another graph object which is the BFS tree of the node with the given int as value
-    Graph DFS(int start);                              // Returns another graph object which is the DFS tree of the node with the given int as value
-    std::unordered_map<int, int> Dijisktra(int start); // Gives a map in which the index is the node value and the value of the index is the shortest distance of it from the start
-    Graph Prim();
+    void add_node(int data);                          // Adds a node in the adjacency list with the value given as parameter
+    void add_edge(int data1, int data2, int weight);  // Adds a weighted edge between the two nodes with the values as given in the argument
+    bool are_adjacent(int data1, int data2);          // Function to check whether two nodes with the given values are adjacent or not
+    int getWeight(int data1, int data2);              // Function to return the weight of edge between nodes given the node
+    void printGraph();                                // Prints the entire graph (with weights in parenthesis)
+    Graph BFS(int start);                             // Returns another graph object which is the BFS tree of the node with the given int as value
+    Graph DFS(int start);                             // Returns another graph object which is the DFS tree of the node with the given int as value
+    std::unordered_map<int, int> Dijkstra(int start); // Gives a map in which the index is the node value and the value of the index is the shortest distance of it from the start
+    Graph Prim();                                     // Returns a minimum spanning tree for the graph
 };
 
 Graph::Graph() // The default constructor
@@ -64,7 +66,7 @@ Graph::Graph(const Graph &graph)
     for (auto node : graph.nodes)
     {
         Node *newNode = new Node(node->val);
-        nodes.push_back(newNode); // Add the node to the new graph
+        nodes.push_back(newNode);    // Add the node to the new graph
         nodeMapping[node] = newNode; // Map the original node to the copied node
     }
 
@@ -86,8 +88,6 @@ Graph::Graph(const Graph &graph)
         }
     }
 }
-
-
 
 void Graph::add_node(int data)
 {
@@ -122,14 +122,16 @@ bool Graph::are_adjacent(int data1, int data2)
     {
         for (auto neighbor : node1->neighbors) // Check if node2 is a neighbor of node1
         {
-            if (neighbor.first == node2) return true;
+            if (neighbor.first == node2)
+                return true;
         }
     }
 
     return false; // Nodes are not adjacent or at least one node does not exist in the graph
 }
 
-int Graph::getWeight(int data1, int data2){
+int Graph::getWeight(int data1, int data2)
+{
     Node *node1 = findNode(data1);
     Node *node2 = findNode(data2);
     for (auto &neighbor : node1->neighbors)
@@ -211,7 +213,7 @@ Graph Graph::DFS(int start)
     return result;
 }
 
-std::unordered_map<int, int> Graph::Dijisktra(int start)
+std::unordered_map<int, int> Graph::Dijkstra(int start)
 {
     Node *start_node = findNode(start);    // Finds the starting node
     std::unordered_map<int, int> result;   // The map which we will return
@@ -244,38 +246,57 @@ std::unordered_map<int, int> Graph::Dijisktra(int start)
     return result;
 }
 
-Graph Graph::Prim() {
-    std::unordered_set<Node*> visited;
-    visited.insert(nodes[0]); // Start with any node
+Graph Graph::Prim()
+{
+    std::unordered_set<Node *> visited; // Track nodes that have already been added
+    visited.insert(nodes[0]);           // Start with any node
 
-    auto cmp = [](const std::pair<int, std::pair<int, int>>& a, const std::pair<int, std::pair<int, int>>& b) {
+    auto cmp = [](const std::pair<int, std::pair<int, int>> &a, const std::pair<int, std::pair<int, int>> &b)
+    {
         return a.first > b.first;
-    };
+    }; // Defines comparison operator for edges based on their weights
+
+    // This creates a priority queue to store edges (as pairs with weight and connected nodes)
     std::priority_queue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, decltype(cmp)> pq(cmp);
 
     // Add edges from the first node to the priority queue
-    for (const auto& neighbor : nodes[0]->neighbors) {
+    for (const auto &neighbor : nodes[0]->neighbors)
+    {
         pq.push({neighbor.second, {nodes[0]->val, neighbor.first->val}});
     }
 
-    Graph minSpanningTree;
-    while (!pq.empty()) {
-        std::pair<int, std::pair<int, int>> currEdge = pq.top();
+    Graph minSpanningTree; // Initialize the final graph object we will return
+
+    // Main loop
+    while (!pq.empty())
+    {
+        std::pair<int, std::pair<int, int>> currEdge = pq.top(); // Extract the edge with minimum weight
         pq.pop();
 
+        // Extract the nodes of the edge
         int from = currEdge.second.first;
         int to = currEdge.second.second;
 
-        if (visited.find(findNode(to)) == visited.end()) {
+        // If the 'to' node isn't visited, add that in the min spanning tree
+        if (visited.find(findNode(to)) == visited.end())
+        {
             visited.insert(findNode(to));
-            minSpanningTree.add_node(from);
-            minSpanningTree.add_node(to);
+            // Check if the nodes are present already or not and add them correspondingly
+            if (minSpanningTree.findNode(from) == NULL)
+                minSpanningTree.add_node(from);
+            if (minSpanningTree.findNode(to) == NULL)
+                minSpanningTree.add_node(to);
             minSpanningTree.add_edge(from, to, currEdge.first);
 
-            for (const auto& edge : edges) {
-                if (edge.second.first == to && visited.find(findNode(edge.second.second)) == visited.end()) {
+            // Adding more edges to the priority queue
+            for (const auto &edge : edges)
+            {
+                if (edge.second.first == to && visited.find(findNode(edge.second.second)) == visited.end())
+                {
                     pq.push({edge.first, {to, edge.second.second}});
-                } else if (edge.second.second == to && visited.find(findNode(edge.second.first)) == visited.end()) {
+                }
+                else if (edge.second.second == to && visited.find(findNode(edge.second.first)) == visited.end())
+                {
                     pq.push({edge.first, {to, edge.second.first}});
                 }
             }
@@ -283,6 +304,8 @@ Graph Graph::Prim() {
     }
     return minSpanningTree;
 }
+
+// Making a subclass for directed graphs
 
 class DirectedGraph : public Graph
 {
@@ -299,42 +322,46 @@ public:
     }
     DirectedGraph();
     DirectedGraph(const DirectedGraph &graph);
-    int Ford_Fulkerson(int source, int sink);
+    void delete_node(int data);
+    std::pair<int, DirectedGraph> Ford_Fulkerson(int source, int sink);
     void change_edge_weight(int data1, int data2, int newWeight);
     bool augment(int source, int sink, std::unordered_map<int, int> &parent);
-    int dfsAugment(DirectedGraph &residualGraph, int current, int sink, int minCapacity, std::unordered_map<int,bool> &visited);
+    int dfsAugment(DirectedGraph &residualGraph, int current, int sink, int minCapacity, std::unordered_map<int, bool> &visited);
+    void perfectMatching(int n);
 };
 
-DirectedGraph::DirectedGraph(){
-    num_nodes=0;
-    num_edges=0;
+// The default constructor
+DirectedGraph::DirectedGraph()
+{
+    num_nodes = 0;
+    num_edges = 0;
 }
 
 DirectedGraph::DirectedGraph(const DirectedGraph &graph)
 {
     // Create a map to keep track of the mapping between original nodes and copied nodes
-    std::unordered_map<Node*, Node*> nodeMapping;
+    std::unordered_map<Node *, Node *> nodeMapping;
 
     // Copy the nodes and build the mapping
     for (auto node : graph.nodes)
     {
-        Node* newNode = new Node(node->val);
-        add_node(newNode->val);  // Add the node to the new graph
+        Node *newNode = new Node(node->val);
+        add_node(newNode->val);      // Add the node to the new graph
         nodeMapping[node] = newNode; // Map the original node to the copied node
     }
 
     // Copy the edges using the node mapping
     for (auto node : graph.nodes)
     {
-        Node* originalNode = node;
-        Node* copiedNode = nodeMapping[originalNode];
+        Node *originalNode = node;
+        Node *copiedNode = nodeMapping[originalNode];
 
         // Fetch all edges for the current node in the original graph
-        for (const auto& neighbor : originalNode->neighbors)
+        for (const auto &neighbor : originalNode->neighbors)
         {
-            Node* neighborNode = neighbor.first;
+            Node *neighborNode = neighbor.first;
             int weight = neighbor.second;
-            Node* copiedNeighborNode = nodeMapping[neighborNode];
+            Node *copiedNeighborNode = nodeMapping[neighborNode];
 
             // Add the edge to the new graph
             add_edge(copiedNode->val, copiedNeighborNode->val, weight);
@@ -342,8 +369,40 @@ DirectedGraph::DirectedGraph(const DirectedGraph &graph)
     }
 }
 
+void DirectedGraph::delete_node(int data)
+{
+    // Find the node to be deleted
+    Node *nodeToDelete = findNode(data);
+
+    if (nodeToDelete)
+    {
+        // Remove the node from the 'nodes' vector
+        auto it = std::find(nodes.begin(), nodes.end(), nodeToDelete);
+        if (it != nodes.end())
+        {
+            nodes.erase(it);
+        }
+
+        // Remove edges connected to the node
+        for (Node *node : nodes)
+        {
+            auto &neighbors = node->neighbors;
+            neighbors.erase(std::remove_if(neighbors.begin(), neighbors.end(),
+                                           [nodeToDelete](const std::pair<Node *, int> &neighbor)
+                                           {
+                                               return neighbor.first == nodeToDelete;
+                                           }),
+                            neighbors.end());
+        }
+
+        // Delete the node
+        delete nodeToDelete;
+    }
+}
+
 void DirectedGraph::change_edge_weight(int data1, int data2, int weight)
 {
+    // Find the nodes
     Node *node1 = findNode(data1);
     Node *node2 = findNode(data2);
     bool edgeExists = false;
@@ -365,23 +424,31 @@ void DirectedGraph::change_edge_weight(int data1, int data2, int weight)
     }
 }
 
-int DirectedGraph::dfsAugment(DirectedGraph &residualGraph, int current, int sink, int minCapacity, std::unordered_map<int,bool> &visited)
+// Function to perform a depth-first search on the residual graph to find an augmenting path
+// from 'current' to 'sink' with a minimum capacity of 'minCapacity'
+int DirectedGraph::dfsAugment(DirectedGraph &residualGraph, int current, int sink, int minCapacity, std::unordered_map<int, bool> &visited)
 {
     visited[current] = true;
 
+    // If we have reached the sink node, return the minimum capacity found along the path
     if (current == sink)
     {
         return minCapacity;
     }
 
+    // Iterate through neighbors of the current node in the residual graph
     for (auto &neighbor : residualGraph.findNode(current)->neighbors)
     {
         int next = neighbor.first->val;
         int capacity = neighbor.second;
 
+        // If the neighbor is unvisited and the edge has remaining capacity
         if (!visited[next] && capacity > 0)
         {
+            // Recursively find the bottleneck value for the augmenting path
             int bottleneck = dfsAugment(residualGraph, next, sink, std::min(minCapacity, capacity), visited);
+
+            // If an augmenting path is found update residual graph and return bottleneck
             if (bottleneck > 0)
             {
                 residualGraph.change_edge_weight(current, next, capacity - bottleneck);
@@ -391,25 +458,66 @@ int DirectedGraph::dfsAugment(DirectedGraph &residualGraph, int current, int sin
         }
     }
 
+    // If no augmenting path found return 0
     return 0;
 }
 
-int DirectedGraph::Ford_Fulkerson(int source, int sink)
+// Function to find the maximum flow in a graph using the Ford-Fulkerson algorithm
+std::pair<int, DirectedGraph> DirectedGraph::Ford_Fulkerson(int source, int sink)
 {
+    // Create a copy of the original graph as the residual graph
     DirectedGraph residualGraph(*this);
+
+    // Initialize the total maximum flow to 0
     int maxFlow = 0;
     int bottleneck;
-        residualGraph.printGraph();
+
+    // Main loop
     while (true)
     {
-        std::unordered_map<int,bool> visited;
-        for(auto &i:nodes){
-            visited[i->val]=false;
+        // Initialize a map to keep track of visited nodes in DFS
+        std::unordered_map<int, bool> visited;
+        for (auto &i : nodes)
+        {
+            visited[i->val] = false;
         }
-        bottleneck = dfsAugment(residualGraph, source, sink, INT_MAX, visited);
-        if (bottleneck == 0) break;
-        maxFlow += bottleneck;
+
+        bottleneck = dfsAugment(residualGraph, source, sink, INT_MAX, visited); // Find an augmenting path and the corresponding bottleneck value
+        if (bottleneck == 0)
+            break;             // If no more augmenting paths can be found, exit the loop
+        maxFlow += bottleneck; // Update the maxFlow variable
     }
 
-    return maxFlow;
+    return std::make_pair(maxFlow, residualGraph);
+}
+
+void DirectedGraph::perfectMatching(int n)
+{
+    DirectedGraph flowGraph(*this);
+    // Adding the source and sink nodes
+    flowGraph.add_node(0);
+    flowGraph.add_node(2 * n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        flowGraph.add_edge(0, i, 1);
+        flowGraph.add_edge(n + i, 2 * n + 1, 1);
+    }
+    // Getting the residual graph using ford-fulkerson algo
+    DirectedGraph residualGraph = flowGraph.Ford_Fulkerson(0, 2 * n + 1).second;
+    residualGraph.delete_node(0);
+    residualGraph.delete_node(2 * n + 1);
+    std::cout << "--------------------\n";
+    std::cout << "|Set 1    |    Set 2|\n";
+    std::cout << "--------------------\n";
+    for (int i = 1; i <= n; i++)
+    {
+        for (auto neighbor : residualGraph.findNode(i)->neighbors)
+        {
+            if (neighbor.second == 0)
+            {
+                std::cout << "|  " << i << "      |      " << neighbor.first->val - n << "  |\n";
+            }
+        }
+    }
+    std::cout << "--------------------\n";
 }
